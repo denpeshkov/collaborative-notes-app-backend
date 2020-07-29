843f4bfe5cbc699351d3fe9fd0eefadf3780dfaa
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-07-29 18:35:35 +0300

    changed .pom description, added README.md updated dependencies

--- service-registry/src/main/java/com/github/denpeshkov/serviceregistry/ServiceRegistry.java
+++ service-registry/src/main/java/com/github/denpeshkov/serviceregistry/ServiceRegistry.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//@EnableEurekaServer
+@EnableEurekaServer
 public class ServiceRegistry {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ServiceRegistry.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceRegistry.class, args);
+  }
 }
