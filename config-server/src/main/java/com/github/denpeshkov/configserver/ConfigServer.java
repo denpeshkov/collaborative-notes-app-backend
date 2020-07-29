15428b6e305c580b42874371022f86549b360ad7
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-07-30 00:02:13 +0300

    added docs

--- config-server/src/main/java/com/github/denpeshkov/configserver/ConfigServer.java
+++ config-server/src/main/java/com/github/denpeshkov/configserver/ConfigServer.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/** Service that manages configurations fot other services */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServer {
+  /**
+   * Main method used to start the service
+   *
+   * @param args standard cli parameters
+   */
   public static void main(String[] args) {
     SpringApplication.run(ConfigServer.class, args);
   }
