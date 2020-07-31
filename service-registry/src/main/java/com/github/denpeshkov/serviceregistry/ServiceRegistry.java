b82d1b8b8bfd82e762d02f3d4d0c9f68c2f7ce74
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-07-31 18:07:42 +0300

    added docs

--- service-registry/src/main/java/com/github/denpeshkov/serviceregistry/ServiceRegistry.java
+++ service-registry/src/main/java/com/github/denpeshkov/serviceregistry/ServiceRegistry.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/** Main class of service-registry */
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistry {
+  /**
+   * Main method used to start service-registry
+   *
+   * @param args CLI parameters
+   */
   public static void main(String[] args) {
     SpringApplication.run(ServiceRegistry.class, args);
   }
