79074c8994e47ce89d4cf474c8699efbc821cb7b
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-08-23 22:45:22 +0300

    moved authentication stub to api-gateway

--- api-gateway/src/main/java/com/github/denpeshkov/apigateway/WebSecurityConfig.java
+++ api-gateway/src/main/java/com/github/denpeshkov/apigateway/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.github.denpeshkov.authentication;
+package com.github.denpeshkov.apigateway;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
