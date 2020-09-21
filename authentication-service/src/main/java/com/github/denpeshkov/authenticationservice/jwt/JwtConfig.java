ff579047dbc8aa95c9ebf56962962519cc467b46
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-09-21 21:44:53 +0300

    added config to bootstrap altered some ode issues

--- authentication-service/src/main/java/com/github/denpeshkov/authenticationservice/jwt/JwtConfig.java
+++ authentication-service/src/main/java/com/github/denpeshkov/authenticationservice/jwt/JwtConfig.java
@@ -1,17 +1,19 @@
 package com.github.denpeshkov.authenticationservice.jwt;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.Period;
 
 /** Configurations for JWT tokens */
+@Configuration
 @ConfigurationProperties(prefix = "security.jwt")
-@ConstructorBinding
 @Validated
 public class JwtConfig {
+
   /** Authorization header */
   @NotBlank private String header;
 
@@ -19,10 +21,26 @@ public class JwtConfig {
   @NotBlank private String schema;
 
   /** Duration since token creation date after which token is expired */
-  private Period expirationPeriod;
+  @NotNull private Period expirationPeriod;
 
   /** Token secret */
   @NotBlank private String secret;
+
+  public void setHeader(String header) {
+    this.header = header;
+  }
+
+  public void setSchema(String schema) {
+    this.schema = schema;
+  }
+
+  public void setExpirationPeriod(Period expirationPeriod) {
+    this.expirationPeriod = expirationPeriod;
+  }
+
+  public void setSecret(String secret) {
+    this.secret = secret;
+  }
 
   public String getHeader() {
     return header;
