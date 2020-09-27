897295b5a2d3808a42403ac3b7605e88edaf661b
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-09-27 18:57:49 +0300

    added REST exception handling

--- api-gateway/src/main/java/com/github/denpeshkov/apigateway/security/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/github/denpeshkov/apigateway/security/security/SecurityConfig.java
@@ -1,15 +1,17 @@
 package com.github.denpeshkov.apigateway.security.security;
 
+import com.github.denpeshkov.apigateway.security.exception.RestAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Configuration
 public class SecurityConfig {
-  @Bean
-  AuthenticationEntryPoint authenticationEntryPoint() {
-    return new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED);
-  }
+    @Bean
+    AuthenticationEntryPoint authenticationEntryPoint(
+            @Qualifier("handlerExceptionResolver") HandlerExceptionResolver exceptionResolver) {
+        return new RestAuthenticationEntryPoint(exceptionResolver);
+    }
 }
