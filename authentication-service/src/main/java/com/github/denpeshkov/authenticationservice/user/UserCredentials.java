92711a54f15602c7e44c2a8f123f1d6dde9bb3fc
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-10-14 20:56:01 +0300

    refactored UserCredentials and added unit test UserCredentialsTest

--- authentication-service/src/main/java/com/github/denpeshkov/authenticationservice/user/UserCredentials.java
+++ authentication-service/src/main/java/com/github/denpeshkov/authenticationservice/user/UserCredentials.java
@@ -4,28 +4,47 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRawValue;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collections;
 import java.util.Objects;
 import java.util.Set;
 
 /** Represent user's credentials during authentication and registration */
-@JsonIgnoreProperties(
-    ignoreUnknown = true,
-    value = {"authorities"},
-    allowGetters = true)
+@JsonIgnoreProperties(ignoreUnknown = true, allowGetters = true)
+@Table("USER_CREDENTIALS")
 public class UserCredentials {
+  /** Id used to persists entity in DB */
+  private @Id Long id;
   /** user's username */
-  private final String username;
-
+  private String username;
   /** user's password */
-  private final String password;
+  private String password;
+  /**
+   * user's roles
+   *
+   * <p><b>not used at the moment !!! so {@link Transient}
+   */
+  @JsonRawValue @Transient private Set<SimpleGrantedAuthority> authorities;
 
-  /** user's roles */
-  @JsonRawValue private final Set<GrantedAuthority> authorities;
+  /**
+   * Creates entity from {@link UserDetails} instance to work with Spring Security abstractions
+   *
+   * @param userDetails {@link UserDetails} instance
+   */
+  public UserCredentials(UserDetails userDetails) {
+    username = userDetails.getUsername();
+    password = userDetails.getPassword();
+    authorities = (Set<SimpleGrantedAuthority>) userDetails.getAuthorities();
+  }
 
   @JsonCreator
+  @PersistenceConstructor
   public UserCredentials(
       @JsonProperty("username") String username, @JsonProperty("password") String password) {
     this.username = username;
@@ -33,9 +52,23 @@ public class UserCredentials {
     authorities = Collections.emptySet();
   }
 
-  public UserCredentials(String username, String password, Set<GrantedAuthority> authorities) {
+  // constructor used to persist entity
+  public UserCredentials(
+      String username, String password, Set<SimpleGrantedAuthority> authorities) {
     this.username = username;
     this.password = password;
+    this.authorities = authorities;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setAuthorities(Set<SimpleGrantedAuthority> authorities) {
     this.authorities = authorities;
   }
 
@@ -47,7 +80,7 @@ public class UserCredentials {
     return password;
   }
 
-  public Set<GrantedAuthority> getAuthorities() {
+  public Set<SimpleGrantedAuthority> getAuthorities() {
     return authorities;
   }
 
--- authentication-service/src/test/java/com/github/denpeshkov/authenticationservice/user/UserCredentialsTest.java
+++ authentication-service/src/test/java/com/github/denpeshkov/authenticationservice/user/UserCredentialsTest.java
@@ -1,39 +1,86 @@
 package com.github.denpeshkov.authenticationservice.user;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class UserCredentialsTest {
-  private static final ObjectMapper mapper = new ObjectMapper();
-  private static String request;
+  private JacksonTester<UserCredentials> jacksonTester;
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+
+  private static UserCredentials userCredentialsWithAuthorities;
+  private static UserCredentials userCredentialsWithoutAuthorities;
+  private static URI userCredentialsWithAuthoritiesJson;
+  private static URI userCredentialsWithoutAuthoritiesJson;
+
+  @BeforeAll
+  static void beforeAll() throws URISyntaxException {
+    // to preserve insertion order
+    Set<SimpleGrantedAuthority> authorities = new LinkedHashSet<>();
+    authorities.add(new SimpleGrantedAuthority("ADMIN"));
+    authorities.add(new SimpleGrantedAuthority("USER"));
+
+    userCredentialsWithAuthorities = new UserCredentials("root", "root", authorities);
+
+    userCredentialsWithoutAuthorities = new UserCredentials("root", "root");
+
+    userCredentialsWithAuthoritiesJson =
+        UserCredentialsTest.class
+            .getClassLoader()
+            .getResource("UserCredentialsWithAuthorities.json")
+            .toURI();
+
+    userCredentialsWithoutAuthoritiesJson =
+        UserCredentialsTest.class
+            .getClassLoader()
+            .getResource("UserCredentialsWithoutAuthorities.json")
+            .toURI();
+  }
 
   @BeforeEach
-  void setUp() {
-    request = "{\"username\":\"aaa\", \"password\":\"aaa\"}";
+  void beforeEach() {
+    JacksonTester.initFields(this, objectMapper);
   }
 
   @Test
-  void testDesiralization() throws JsonProcessingException {
-    UserCredentials userCredentials = mapper.readValue(request, UserCredentials.class);
+  void testDesiralizationWithoutAuthorities() throws IOException {
+    String json = Files.readString(Paths.get(userCredentialsWithoutAuthoritiesJson));
 
-    System.out.println(userCredentials);
+    assertThat(jacksonTester.parse(json))
+        .isEqualToComparingFieldByField(userCredentialsWithoutAuthorities);
   }
 
   @Test
-  void testSerialization() throws JsonProcessingException {
-    UserCredentials userCredentials =
-        new UserCredentials(
-            "aa",
-            "aaaaa",
-            Set.of(new SimpleGrantedAuthority("ROLE_1"), new SimpleGrantedAuthority("ROLE_2")));
+  void testDesiralizationWithAuthorities() throws IOException {
+    String json = Files.readString(Paths.get(userCredentialsWithAuthoritiesJson));
 
-    System.out.println(userCredentials);
+    assertThat(jacksonTester.parse(json))
+        .isEqualToComparingFieldByField(userCredentialsWithAuthorities);
+  }
 
-    System.out.println(mapper.writeValueAsString(userCredentials));
+  @Test
+  void testSerializationWithoutAuthorities() throws IOException {
+    assertThat(jacksonTester.write(userCredentialsWithoutAuthorities))
+        .isStrictlyEqualToJson(new File(userCredentialsWithoutAuthoritiesJson));
+  }
+
+  @Test
+  void testSerializationWithAuthorities() throws IOException {
+    assertThat(jacksonTester.write(userCredentialsWithAuthorities))
+        .isStrictlyEqualToJson(new File(userCredentialsWithAuthoritiesJson));
   }
 }
