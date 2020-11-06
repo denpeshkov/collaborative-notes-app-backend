97564352ab3cb7bc23d74df561d96e71eee143df
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-11-06 16:28:10 +0300

    added JwtConfig validation and @ConfigurationProperties

--- authentication-service/src/main/java/com/github/denpeshkov/authenticationservice/security/SecurityConfig.java
+++ authentication-service/src/main/java/com/github/denpeshkov/authenticationservice/security/SecurityConfig.java
@@ -1,16 +1,11 @@
 package com.github.denpeshkov.authenticationservice.security;
 
-import com.github.denpeshkov.authenticationservice.exception.RestAuthenticationEntryPoint;
 import com.github.denpeshkov.commons.security.jwt.JwtConfig;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.validation.annotation.Validated;
 
 @Configuration
 public class SecurityConfig {
@@ -20,26 +15,9 @@ public class SecurityConfig {
   }
 
   @Bean
-  AuthenticationEntryPoint authenticationEntryPoint(
-      @Qualifier("handlerExceptionResolver") HandlerExceptionResolver exceptionResolver) {
-    return new RestAuthenticationEntryPoint(exceptionResolver);
-  }
-
-  @Bean
+  @Validated
+  @ConfigurationProperties(prefix = "security.jwt")
   JwtConfig jwtConfig() {
     return new JwtConfig();
-  }
-
-  /**
-   * Configures {@link org.springframework.http.converter.json.Jackson2ObjectMapperBuilder} to
-   * serialize {@link SimpleGrantedAuthority}
-   *
-   * @return Jackson customizer
-   */
-  @Bean
-  @Order(1)
-  Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-    return customizer ->
-        customizer.mixIn(SimpleGrantedAuthority.class, SimpleGrantedAuthorityMixin.class);
   }
 }
