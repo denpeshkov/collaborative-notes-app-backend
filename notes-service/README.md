4b41b9806de1d4810fefb5166d46b7715f54ea4a
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-12-10 01:07:33 +0300

    initial commit

---
+++ notes-service/README.md
@@ -0,0 +1,3 @@
+# Notes service.
+
+Notes management
---
+++ notes-service/src/test/java/com/github/denpeshkov/notesservice/NotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.denpeshkov.notesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
