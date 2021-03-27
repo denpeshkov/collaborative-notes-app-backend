5fbd657d54c796cb9f819f45a852c556d27533d8
Peshkov <peshkovdenis1985@gmail.com>
2021-03-27 22:17:34 +0300

    cors

--- api-gateway/src/main/java/com/github/denpeshkov/apigateway/security/security/WebSecurityConfigurer.java
+++ api-gateway/src/main/java/com/github/denpeshkov/apigateway/security/security/WebSecurityConfigurer.java
@@ -25,7 +25,9 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf()
+    http.cors()
+        .and()
+        .csrf()
         .disable()
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
